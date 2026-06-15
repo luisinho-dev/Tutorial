0b8e6d64c5953c023597badd0e8bb4d3d2e36238
luisinho-dev <luisinho-dev@gmail.com>
2026-06-15 16:56:26 -0300

    Mod de gravidade aleatoria pronto

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,28 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayerObject.hpp>
+#include <cstdlib>
 
 using namespace geode::prelude;
+
+float tempoDecorrido = 0.0f;
+float proximaInversao = 8.0f;
+
+class $modify(PlayerObject) {
+    void update(float dt) {
+        PlayerObject::update(dt);
+
+        if (this == m_gameLayer->m_player1) {
+            tempoDecorrido += dt;
+
+            if (tempoDecorrido >= proximaInversao) {
+                this->flipGravity(!m_isUpsideDown, true);
+
+                Notification::create("GRAVIDADE ALTERADA!", NotificationIcon::Warning)->show();
+
+                tempoDecorrido = 0.0f;
+                proximaInversao = 5.0f + static_cast<float>(rand() % 11);
+            }
+        }
+    }
+};
+
